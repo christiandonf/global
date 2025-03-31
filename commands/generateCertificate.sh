0cdd878c19ef9158ecb471baa0677b3946399b27
Christian <christian.nascimento.falcao@gmail.com>
2025-03-31 13:39:57 +0000

    Commands for new Certificates

---
+++ commands/generateCertificate.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Gera novos certificados SSL com desafio DNS
+DOMAINS=(-d nexuswebdigital.com -d *.nexuswebdigital.com)
+EMAIL="markin-work@gmail.com"
+COMPOSER_FILE="/var/opt/global/docker-compose.yml"
+docker compose -f "$COMPOSER_FILE" run --rm sslbot certonly \
+  --manual \
+  --preferred-challenges dns \
+  "${DOMAINS[@]}" \
+  --email "$EMAIL" \
+  --agree-tos \
+  --no-eff-email
+
+# Verifica se o comando anterior foi executado com sucesso
+if [ $? -eq 0 ]; then
+  echo -e "\n✅ Certificado gerado com sucesso!"
+else
+  echo -e "\n❌ Falha na geração do certificado!"
+  exit 1
+fi
---
+++ commands/renewCertificate.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Renova certificados SSL expirados
+echo "Iniciando renovação de certificados..."
+
+COMPOSER_FILE="/var/opt/global/docker-compose.yml"
+
+docker compose -f "$COMPOSER_FILE" run --rm sslbot renew
+
+if [ $? -eq 0 ]; then
+  echo -e "\n✅ Certificados renovados com sucesso!"
+else
+  echo -e "\n❌ Falha na renovação dos certificados!"
+  exit 1
+fi
